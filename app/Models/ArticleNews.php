aa4aa77330f25b3cb48a8d8943f58806ae6176bf
daffa <daffaryasatya28@gmail.com>
2024-08-27 14:02:12 +0700

    mengatur ORM Relationship antar table

--- app/Models/ArticleNews.php
+++ app/Models/ArticleNews.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ArticleNews extends Model
@@ -19,4 +20,13 @@ class ArticleNews extends Model
         'author_id',
         'is_featured',
     ];
+
+    public function Category(): BelongsTo
+    {
+        return $this->belongsTo(ArticleNews::class, 'category_id');
+    }
+    public function Author(): BelongsTo
+    {
+        return $this->belongsTo(ArticleNews::class, 'author_id');
+    }
 }
