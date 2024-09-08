6bd1943ede197b89ccb769b105c19e776f5cc038
daffa <daffaryasatya28@gmail.com>
2024-09-08 08:54:24 +0700

    menampilkan category pada frontend

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
     //
     public function index(){
-        return view('front.index');
+        //mengambil semua data di category
+        $categories = Category::all();
+        //mengembalikan ke halaman front, dan membyuat array berisi $categories
+        return view('front.index', compact('categories'));
     }
 }
