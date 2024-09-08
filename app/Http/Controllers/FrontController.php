af89ebf77e095307d13120be832622a3437e389f
daffa <daffaryasatya28@gmail.com>
2024-09-08 22:13:50 +0700

    tampilkan data news dan authors

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ArticleNews;
+use App\Models\Author;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -11,7 +13,17 @@ class FrontController extends Controller
     public function index(){
         //mengambil semua data di category
         $categories = Category::all();
+
+        //mengambil data artricle yang berafiliasi juga dengan article
+        $articles = ArticleNews::with(['category'])
+        //mengambil data yang not_featured saja
+        ->where('is_featured','not_featured')
+        ->latest()
+        ->take(3)
+        ->get();
+
+        $authors = Author::all();
         //mengembalikan ke halaman front, dan membyuat array berisi $categories
-        return view('front.index', compact('categories'));
+        return view('front.index', compact('categories','articles','authors'));
     }
 }
