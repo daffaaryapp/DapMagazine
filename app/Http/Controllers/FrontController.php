1657d5d3da09c3581a01ece1748d7305208db159
daffa <daffaryasatya28@gmail.com>
2024-09-11 11:48:28 +0700

    tampilkan featured news dan iklan

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\ArticleNews;
 use App\Models\Author;
+use App\Models\BannerAdvertisement;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -22,8 +23,22 @@ class FrontController extends Controller
         ->take(3)
         ->get();
 
+        $featured_articles = ArticleNews::with(['category'])
+        //mengambil data yang not_featured saja
+        ->where('is_featured','featured')
+        ->inRandomOrder()
+        ->take(3)
+        ->get();
+
         $authors = Author::all();
+
+        $bannerads = BannerAdvertisement::where('is_active', 'active')
+        ->where('type','banner')
+        ->inRandomOrder()
+        // ->take(1)
+        ->first();
+
         //mengembalikan ke halaman front, dan membyuat array berisi $categories
-        return view('front.index', compact('categories','articles','authors'));
+        return view('front.index', compact('categories','articles','authors','featured_articles','bannerads'));
     }
 }
